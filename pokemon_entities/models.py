e09fd1f6ab02be03f5b69dc4ecbf4d9dfd285eec
Viktor Shishkalov <viktorshish@gmail.com>
2023-11-10 00:02:51 +0300

    Add description and title_en in Pokemon model

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -2,8 +2,10 @@ from django.db import models  # noqa F401
 
 
 class Pokemon(models.Model):
-    title = models.CharField(max_length=200)
+    title_ru = models.CharField(max_length=200)
+    title_en = models.CharField(max_length=200, null=True, blank=True)
     image = models.ImageField(upload_to='pokemons', null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return f'{self.title}'
