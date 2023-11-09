744402493345ffb04c2e936bdc52af16e19f1f29
Viktor Shishkalov <viktorshish@gmail.com>
2023-11-10 00:14:55 +0300

    Add japan title in Pokemon model

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -4,6 +4,7 @@ from django.db import models  # noqa F401
 class Pokemon(models.Model):
     title_ru = models.CharField(max_length=200)
     title_en = models.CharField(max_length=200, null=True, blank=True)
+    title_jp = models.CharField(max_length=200, null=True, blank=True)
     image = models.ImageField(upload_to='pokemons', null=True, blank=True)
     description = models.TextField(null=True, blank=True)
 
