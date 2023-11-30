46de6f96c80104a106647c5f29a6aa59396c0535
Viktor Shishkalov <viktorshish@gmail.com>
2023-11-30 21:27:46 +0300

    Reduced the number of requests

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -50,13 +50,13 @@ def show_all_pokemons(request):
                 'img_url': image_url,
             })
 
-        for pokemon_entity in pokemon_entities:
-            entity_image_url = pokemon_entity.pokemon.image.url if pokemon_entity.pokemon.image else None
-            add_pokemon(
-                folium_map, pokemon_entity.lat,
-                pokemon_entity.lon,
-                request.build_absolute_uri(entity_image_url),
-            )
+    for pokemon_entity in pokemon_entities:
+        entity_image_url = pokemon_entity.pokemon.image.url if pokemon_entity.pokemon.image else None
+        add_pokemon(
+            folium_map, pokemon_entity.lat,
+            pokemon_entity.lon,
+            request.build_absolute_uri(entity_image_url),
+        )
 
     return render(request, 'mainpage.html', context={
         'map': folium_map._repr_html_(),
